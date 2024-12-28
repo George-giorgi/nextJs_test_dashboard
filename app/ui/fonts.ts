4bc9250cddd35c72e6ff2e8b1021e32d08fd9db9
Giorgi <georgegiorgika@gmail.com>
2024-12-28 14:03:26 +0000

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Lusitana } from "next/font/google";
+
+// font 1 main
+export const inter = Inter({ subsets: ["latin"] });
+
+// specificFont1
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
